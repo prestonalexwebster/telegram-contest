f30843285e1d165eb92c73d2b669ea89b1a4d3ac
ASAnokhin <booolbash@gmail,.com>
2019-03-13 17:45:09 +0300

    replacing parcel by webpack todo: fix chart_data.json loading

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+
+module.exports = {
+    entry: {
+        app: './src/index.js'
+    },
+    devtool: 'inline-source-map',
+    mode: 'development',
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                loader: 'babel-loader',
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Charts'
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
