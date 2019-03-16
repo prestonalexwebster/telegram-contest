ac11ebd3ca21026c004ec91a8612fd2f9c01ff2e
Alex <prestonalexwebster@gmail.com>
2019-03-16 19:05:29 +0300

    adding scale range logic

--- src/components/controlled-chart-preview/controlled-chart-preview.js
+++ src/components/controlled-chart-preview/controlled-chart-preview.js
@@ -6,6 +6,12 @@ import {seekerSelector} from '../../selectors/points-selectors';
 import {detailedChartSize, previewChartHeight} from "../../constants/charts-size";
 import PureDomRenderer from "../../core/renderers/pure-dom-renderer";
 
+const alignInRange = (value, minV, maxV) => {
+    return Math.min(maxV, Math.max(minV, value));
+};
+
+const minRangeWidth = 30/detailedChartSize;
+
 export default class ControlledChartPreview extends Component {
 
     actions = {
@@ -26,6 +32,10 @@ export default class ControlledChartPreview extends Component {
 
     rightField = new PureDomRenderer('div');
 
+    leftResizer = new PureDomRenderer('div');
+
+    rightResizer = new PureDomRenderer('div');
+
     selector(state){
         const {seekerLeft, seekerWidth} = seekerSelector(state);
         return {seekerLeft,seekerWidth};
@@ -39,24 +49,52 @@ export default class ControlledChartPreview extends Component {
 
     dragPosition = null;
 
-    dragStart = ({clientX}) => {
+    dragTarget = null;
+
+    dragStart = ({clientX, target}) => {
         this.dragged = true;
         this.dragPosition = clientX;
+        this.dragTarget = target;
     };
 
     touchStart = ({changedTouches}) => {
         this.dragStart(changedTouches[0]);
     };
 
+
+    moveSeeker(diff){
+        const {seekerLeft, seekerWidth} = this.attributes;
+        const xStart = alignInRange((seekerLeft+diff)/detailedChartSize, 0, 1-seekerWidth/detailedChartSize);
+        const xEnd = xStart + seekerWidth/detailedChartSize;
+        this.actions.setRange([xStart, xEnd]);
+    }
+
+    moveLeftBorder(diff){
+        const {seekerLeft, seekerWidth} = this.attributes;
+        const xStart = alignInRange((seekerLeft+diff)/detailedChartSize, 0, (seekerLeft+seekerWidth)/detailedChartSize-minRangeWidth );
+        const xEnd = (seekerLeft + seekerWidth)/detailedChartSize; //remains
+        this.actions.setRange([xStart, xEnd]);
+    }
+
+    moveRightBorder(diff){
+        const {seekerLeft,seekerWidth} = this.attributes;
+        const xStart = seekerLeft/detailedChartSize;//remains
+        const xEnd = alignInRange(xStart + (seekerWidth + diff)/detailedChartSize, xStart+minRangeWidth, 1);
+        this.actions.setRange([xStart, xEnd]);
+    }
+
     drag = ({clientX}) => {
         if(!this.dragged) {
             return;
         }
         const diff = clientX - this.dragPosition;
-        const {seekerLeft, seekerWidth} = this.attributes;
-        const xStart = Math.max(0,Math.min((seekerLeft+diff)/detailedChartSize,1-seekerWidth/detailedChartSize));
-        const xEnd = xStart + seekerWidth/detailedChartSize;
-        this.actions.setRange([xStart, xEnd]);
+        if(this.dragTarget === this.seeker.ref){
+            this.moveSeeker(diff);
+        }else if(this.dragTarget === this.leftResizer.ref){
+            this.moveLeftBorder(diff);
+        }else if(this.dragTarget === this.rightResizer.ref){
+            this.moveRightBorder(diff);
+        }
         this.dragPosition = clientX;
     };
 
@@ -69,26 +107,51 @@ export default class ControlledChartPreview extends Component {
             return;
         }
         this.dragged = false;
+        this.dragTarget = null;
+        this.dragPosition = null;
     };
 
     touchEnd = ({changedTouches}) => {
         this.dragEnd(changedTouches[0]);
     };
 
+    renderLeftResizer(){
+        return this.leftResizer
+            .style('height', `${previewChartHeight-6}px`)
+            .style('width', '15px')
+            .style('background-color', 'rgba(119, 171, 207, 0.25)')
+            .style('position', 'absolute')
+            .style('left', '0px');
+    }
+
+    renderRightResizer(){
+        return this.rightResizer
+            .style('height', `${previewChartHeight-6}px`)
+            .style('width', '15px')
+            .style('background-color', 'rgba(119, 171, 207, 0.25)')
+            .style('position', 'absolute')
+            .style('right', '0px');
+    }
+
     renderSeeker(){
         const {seekerWidth} = this.attributes;
         return this.seeker
             .style('display', 'inline-block')
+            .style('position', 'relative')
             .style('box-sizing', 'border-box')
             .style('width', `${seekerWidth}px`)
             .style('height', `${previewChartHeight}px`)
-            .style('border-width', '3px 10px')
+            .style('border-width', '3px 0px')
             .style('border-style', 'solid')
             .style('border-color', 'rgba(119, 171, 207, 0.25)')
             .style('background-color', 'transparent')
             .style('cursor', 'pointer')
             .on('mousedown',this.dragStart)
             .on('touchstart', this.touchStart)
+            .children([
+                this.renderLeftResizer(),
+                this.renderRightResizer()
+            ])
             .render();
     }
 
